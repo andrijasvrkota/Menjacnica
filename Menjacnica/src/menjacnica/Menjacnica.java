7300743aaaa8cc57107212a7bf1baa8e503315be
andrijasvrkota <andrija.svrkota@gmail.com>
2016-03-29 22:33:31 +0200

    Dodata klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
+
+	@Override
+	public void dodajKurs(Kurs kurs, Valuta valuta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(GregorianCalendar datum, Valuta valuta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
