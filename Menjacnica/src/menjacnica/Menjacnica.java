f17991876156d9007ea9ab7d560a079bb6d047ef
andrijasvrkota <andrija.svrkota@gmail.com>
2016-03-31 18:50:14 +0200

    Izmenjena metoda dodajKurs koja ce zameniti postojecu u master grani

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -11,17 +11,14 @@ public class Menjacnica implements MenjacnicaInterfejs{
 	public void dodajKurs(Kurs kurs, Valuta valuta) {
 		
 		boolean postoji=false;
-		int brojKurseva = valuta.getKursevi().size();
-		
-		for (int i = 0; i < brojKurseva; i++) {
-			if(valuta.getKursevi().get(i).getDatum() == kurs.getDatum())
+		for (int i = 0; i < valuta.getKursevi().size(); i++) {
+			if(valuta.getKursevi().get(i).getDatum().equals(kurs.getDatum()))
 				postoji=true;
 		}
-		if(postoji == true)
-			System.out.println("Ovaj datum vec postoji ");
+		if(postoji)
+			System.out.println("Kurs za ovaj datum vec postoji ");
 		else
 			valuta.getKursevi().add(kurs);
-
 	}
 
 	
